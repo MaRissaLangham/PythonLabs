b7fc94e42aee42906f9164a8367c7daecd9eb154
MaRissaLangham <marissalangham@outlook.com>
2023-08-09 01:28:45 -0500

    Created CheckingUsernames.py

---
+++ CheckingUsernames.py
@@ -0,0 +1,9 @@
+currentUsers = ['john', 'james', 'jane', 'jim', 'joe', 'jerry']
+newUsers = ['jim', 'joe', 'jerry', 'josh', 'jake', 'jill']
+
+if newUsers:
+    for user in newUsers:
+        if user.lower() in currentUsers:
+            print("Sorry, the username " + user + " is already taken.")
+        else:
+            print("The username " + user + " is available.")
